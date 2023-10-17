8569ac0c35f340981eab48fa449cea407edaa265
nurdin <taabaldyev.nurdin27@mail.ru>
2023-10-17 20:30:02 +0600

    again

--- review/models.py
+++ review/models.py
@@ -1,1 +1,13 @@
+from account.admin import User
 
+
+class Comment(models.Model):
+    body = models.TextField()
+    author = models.ForeignKey(User, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Like(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Like')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
--- review/views.py
+++ review/views.py
@@ -1,1 +1,37 @@
+from rest_framework import generics
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
+from review.models import Comment, Like
+from review.serializers import CommentSerializer, LikeSerializer
+
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    class LikeView(generics.CreateAPIView):
+        queryset = Like.objects.all()
+        serializer_class = LikeSerializer
+
+        @action(methods=['POST'], detail=True)
+        def like(self, request, pk=None):
+            post = self.get_object()
+            user = request.user
+            try:
+                like = Like.objects.get(post=post, author=user)
+                like.delete()
+                message = 'disliked'
+            except Like.DoesNotExist:
+                like = Like.objects.create(post=post, author=user)
+                like.save()
+                message = 'liked'
+            return Response(message)
+
+    @action(methods=['GET'], detail=True)
+    def comments(self, request, pk=None):
+        post = self.get_object()
+        comments = post.comments.all()
+        serializer = CommentSerializer(comments, Many=True)
+        return Response(serializer.data)
